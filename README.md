a88229d5042a7790c0949b59f4dcb7d0369b7f9f
Daniel Williams <dan@elixirware.com>
2013-02-05 14:04:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Welcome to Texts, where texting meets the web!
+
+Here we will be developing a sweet texting client using only webpages.  How ingenious!
+
+Enjoy!
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Texts::Application.config.secret_token = '907ceaaabba1578695a85a9d399635b908f483cf48a12d7a518381b84eb648242f80ca7192c42051c54c3692f4ec7709b5c677b255a5cfaacf956d45d40c08c2'
