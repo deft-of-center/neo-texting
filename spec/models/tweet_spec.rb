fdb51ad328db7a3d8d45dd23b8078807ba7db255
Daniel Williams <dan@elixirware.com>
2013-02-10 16:41:29 -0500

    Add respond_to test for tweets

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Tweet do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(full_name: "Dan Williams", email: "dan1lkk@neotexting.com", username: "val1", password: "foobar", password_confirmation: "foobar")
+    @user.save
+    @tweet = @user.tweets.create(content: "Character is the tree, reputation the shadow." )
+  end
+
+  subject { @tweet }
+
+  it { should respond_to :content }
+  it { should respond_to :user_id }
 end
